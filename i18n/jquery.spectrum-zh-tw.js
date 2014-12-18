2b68d09281139d3b6908a8cee7549a36ac22b873
Peter Dave Hello <PeterDaveHello@users.noreply.github.com>
2014-12-19 02:00:13 +0800

    Create jquery.spectrum-zh-tw.js

---
+++ i18n/jquery.spectrum-zh-tw.js
@@ -0,0 +1,15 @@
+// Spectrum Colorpicker
+// Traditional Chinese (zh-tw) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["zh-cn"] = {
+        cancelText: "取消",
+        chooseText: "選擇",
+        clearText: "清除"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
